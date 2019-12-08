f6ceb9ccb571b5bcd8f3a1f98a2f609cd1cd9a59
Sergio Ugalde <serch037@gmail.com>
2019-12-08 10:45:40 -0600

    Added catalogues

--- lb-where2/src/repositories/index.ts
+++ lb-where2/src/repositories/index.ts
@@ -1,3 +1,5 @@
 export * from './event.repository';
 export * from './user.repository';
 export * from './user-credentials.repository';
+export * from './event-category.repository';
+export * from './invite-status.repository';
